a6f425d293608dfc544049161b18185bfce4b6c6
ZY <nsndimt@gmail.com>
2014-12-16 21:03:30 +0800

    Version 8

--- src/gmail_plugin_v4/GmailReader.java
+++ src/gmail_plugin_v4/GmailReader.java
@@ -1,73 +1,126 @@
 package gmail_plugin_v1;
 
-import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
+import javax.mail.Address;
+import javax.mail.BodyPart;
+import javax.mail.FetchProfile;
 import javax.mail.Folder;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
-import javax.mail.internet.MimeBodyPart;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class GmailReader {
 
-	public String account;
-	public String password;
-	public Map<String, messageunit> map = new HashMap<String, messageunit>();
+    public String account;
+    public String password;
+    //public List<messageunit> list = new ArrayList<messageunit>();
 
-	public void getmessages() throws MessagingException, IOException {
-		Properties props = System.getProperties();
-		props.setProperty("mail.store.protocol", "imaps");
-		Session session = Session.getDefaultInstance(props, null);
-		Store store = session.getStore("imaps");
-		store.connect("imap.gmail.com", account, password);
-		System.out.println(store.toString());
-		Folder inbox = store.getFolder("Inbox");
-		inbox.open(Folder.READ_ONLY);
-		Message[] tempmessages = inbox.getMessages();
-		for (Message message : tempmessages) {
-			if (message.getContentType().contains("multipart")) {
-				Multipart multiPart = (Multipart) message.getContent();
-				for (int i = 0; i < multiPart.getCount(); i++) {
-					MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(i);
-					messageunit temp = null;
-					if (part.getFileName() != null && part.getFileName() != "") {
-						if ((part.getDisposition() != null)
-								&& (part.getDisposition()
-										.equalsIgnoreCase(Part.ATTACHMENT))) {
-							if (temp == null)
-								temp = new messageunit(message);
-							temp.add(part);
-						}
-					}
-					if (temp != null)
-						map.put(message.getSubject(), temp);
-				}
-			}
+    public List<memfile> getmessages(mysql sql) throws Exception {
+	Properties props = System.getProperties();
+	props.setProperty("mail.store.protocol", "imaps");
+	Session session = Session.getDefaultInstance(props, null);
+	Store store = session.getStore("imaps");
+	store.connect("imap.gmail.com", account, password);
+	// System.out.println(store.toString());
+	Folder inbox = store.getFolder("Inbox");
+	inbox.open(Folder.READ_ONLY);
+	FetchProfile fetchProfile = new FetchProfile();
+	fetchProfile.add(FetchProfile.Item.CONTENT_INFO);
+	fetchProfile.add(FetchProfile.Item.ENVELOPE);
+	fetchProfile.add(FetchProfile.Item.FLAGS);
+	Message[] tempmessages = inbox.getMessages();
+	inbox.fetch(tempmessages, fetchProfile);
+	List<memfile> list = new ArrayList<memfile>();
+	for (Message message : tempmessages) {
+	    // if (message.getContentType().contains("multipart")) {
+	    // Multipart multiPart = (Multipart) message.getContent();
+	    // for (int i = 0; i < multiPart.getCount(); i++) {
+	    // MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(i);
+	    // messageunit temp = null;
+	    // if (part.getFileName() != null && part.getFileName() != "") {
+	    // if ((part.getDisposition() != null)
+	    // && (part.getDisposition()
+	    // .equalsIgnoreCase(Part.ATTACHMENT))) {
+	    // if (temp == null)
+	    // temp = new messageunit(message);
+	    // temp.add(part);
+	    // }
+	    // }
+	    // if (temp != null)
+	    // list.add(temp);
+	    //
+	    // }
+	    // }
+	    list.addAll(getAttachments(message, sql));
+	}
+	return list;
+    }
+
+    public GmailReader(String account, String password) {
+	this.account = account;
+	this.password = password;
+    }
+
+    public List<memfile> getAttachments(Message message, mysql sql)
+	    throws Exception {
+	List<memfile> list = new ArrayList<memfile>();
+	//System.out.println(message.getSubject());
+	SimpleDateFormat dateformat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	String date=dateformat.format(message.getSentDate());
+	String subject=message.getSubject();
+	String from=message.getFrom()[0].toString();
+	from=from.substring(from.indexOf('<')+1, from.length()-1);
+	if (!sql.check(message.getSubject())) {
+	    // Object content = message.getContent();
+	    //System.out.println(message.getSubject());
+	    if (message.getContentType().contains("multipart")) {
+		//System.out.println(message.getSubject());
+		Multipart multipart = (Multipart) message.getContent();
+		for (int i = 0; i < multipart.getCount(); i++) {
+		    list.addAll(getAttachments(multipart.getBodyPart(i), sql,date,subject,from));
 		}
-	}
-
-	public GmailReader(String account, String password) {
-		this.account = account;
-		this.password = password;
-	}
-
-	public List<memfile> getpart() throws IOException, MessagingException {
-		List<memfile> l = new ArrayList<memfile>();
-		for (messageunit m : map.values()) {
-			for (Part p : m.part) {
-				memfile mf=new memfile(p);
-				l.add(mf);
-				//if(mf.data==null) System.out.println("p.data is nulla");
-			}
+	    }
+	    /*if (list.size() != 0) {
+		// System.out.println(list.size());
+		int mailid = sql.addmail(message.getSubject());
+		for (memfile m : list) {
+		    m.mailid = mailid;
+		    m.mailname = message.getSubject();
 		}
-		return l;
+	    }*/
+	    
 	}
+	return list;
+    }
+
+    private List<memfile> getAttachments(BodyPart part, mysql sql,String date,String subject,String from)
+	    throws Exception {
+	List<memfile> list = new ArrayList<memfile>();
+	//System.out.println(part.getFileName());
+	if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())
+		|| StringUtils.isNotBlank(part.getFileName())) {
+	    System.out.println(part.getFileName());
+	    if(!sql.check(subject)){
+	    list.add(new memfile(part, date, subject, from));
+	    }
+	}
+
+	if (part.getContentType().contains("multipart")) {
+	    Multipart multipart = (Multipart) part.getContent();
+	    for (int i = 0; i < multipart.getCount(); i++) {
+		BodyPart bodyPart = multipart.getBodyPart(i);
+		list.addAll(getAttachments(bodyPart, sql,date,subject,from));
+	    }
+	}
+	return list;
+    }
 }
--- src/gmail_plugin_v4/GmailSender.java
+++ src/gmail_plugin_v4/GmailSender.java
@@ -17,60 +17,66 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
+
 public class GmailSender {
- 
-	private String account;
-	private String password;
-	private void addAttachment(Multipart multipart, uploadfile file) throws MessagingException, IOException
-	{
-	    MimeBodyPart messageBodyPart = new MimeBodyPart();
-	    ByteArrayDataSource ds = new ByteArrayDataSource(file.file, "application/octet-stream"); 
-	    messageBodyPart.setDataHandler(new DataHandler(ds));
-	    messageBodyPart.setFileName(file.name);
-	    multipart.addBodyPart(messageBodyPart);
+
+    private String account;
+    private String password;
+
+    private void addAttachment(Multipart multipart, uploadfile file)
+	    throws MessagingException, IOException {
+	MimeBodyPart messageBodyPart = new MimeBodyPart();
+	ByteArrayDataSource ds = new ByteArrayDataSource(file.file,
+		"application/octet-stream");
+	messageBodyPart.setDataHandler(new DataHandler(ds));
+	messageBodyPart.setFileName(file.name);
+	multipart.addBodyPart(messageBodyPart);
+    }
+
+    public GmailSender(String account, String password) {
+	this.account = account;
+	this.password = password;
+    }
+
+    public void send(String address, String subject, String text,
+	    List<uploadfile> files) throws IOException {
+	Properties props = new Properties();
+	props.put("mail.smtp.auth", "true");
+	props.put("mail.smtp.starttls.enable", "true");
+	props.put("mail.smtp.host", "smtp.gmail.com");
+	props.put("mail.smtp.port", "587");
+	System.out.println(account + password + address);
+	Session session = Session.getInstance(props,
+		new javax.mail.Authenticator() {
+		    protected PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(account, password);
+		    }
+		});
+	try {
+
+	    Message message = new MimeMessage(session);
+	    message.setFrom(new InternetAddress(account));
+	    message.setRecipients(Message.RecipientType.TO,
+		    InternetAddress.parse(address));
+	    message.setSubject(subject);
+	    message.setText(text);
+	    Multipart multipart = new MimeMultipart();
+	    for (uploadfile s : files)
+		addAttachment(multipart, s);
+	    message.setContent(multipart);
+	    Transport.send(message);
+	} catch (MessagingException e) {
+	    throw new RuntimeException(e);
 	}
-	public GmailSender(String account,String password)
-	{
-		this.account = account;
-		this.password = password;
-	}
-	public void send(String address,String subject,String text,List<uploadfile> files) throws IOException {
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
-		System.out.println(account+password+address);
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(account, password);
-			}
-		  });
-		try {
- 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(account));
-			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(address));
-			message.setSubject(subject);
-			message.setText(text);
-			Multipart multipart = new MimeMultipart();
-			for(uploadfile s:files) addAttachment(multipart,s);
-			message.setContent(multipart);
-			Transport.send(message); 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    }
 }
-class uploadfile
-{
-	String name;
-	InputStream file;
-	public uploadfile(String name,InputStream file)
-	{
-		this.name=name;
-		this.file=file;
-	}
+
+class uploadfile {
+    String name;
+    InputStream file;
+
+    public uploadfile(String name, InputStream file) {
+	this.name = name;
+	this.file = file;
+    }
 }
